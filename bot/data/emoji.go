71463cace4a4453582fc5b3ff85f88f8701e0465
cyxigo <cyxigo@bk.ru>
2025-07-31 00:24:18 +0300

    Updated emoji in discord app settings and so updated it here

--- bot/data/emoji.go
+++ bot/data/emoji.go
@@ -6,10 +6,10 @@ var (
 	// i cant set all of this as const
 	// so just please dont change them
 	EmojiCykodigo = emoji("cykodigo", "1399481322866741388")
-	EmojiCatr     = emoji("catr", "1399481457977856172")
+	EmojiCatr     = emoji("catr", "1400227174975410327")
 
 	EmojiReactCykodigo = reactionEmoji("cykodigo", "1399481322866741388")
-	EmojiReactCatr     = reactionEmoji("catr", "1399481457977856172")
+	EmojiReactCatr     = reactionEmoji("catr", "1400227174975410327")
 )
 
 func reactionEmoji(name string, id string) string {
