afbc40ed61f9377f99851934f143a4bb2aaa27fb
cyxigo <cyxigo@bk.ru>
2025-08-12 12:32:49 +0300

    I'm so stupid at SQL

--- bot/database/db.go
+++ bot/database/db.go
@@ -36,7 +36,8 @@ func initDB(db *sql.DB, name string) bool {
 		CREATE TABLE IF NOT EXISTS inventory (
 			user_id TEXT NOT NULL,
 			item TEXT NOT NULL,
-			amount INTEGER NOT NULL DEFAULT 1
+			amount INTEGER NOT NULL DEFAULT 1,
+			UNIQUE(item)
 		);
 		CREATE TABLE IF NOT EXISTS effects (
 			user_id TEXT PRIMARY KEY,
